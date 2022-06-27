4ebb97a5cb30b73290dc50f1a6e04047602cd292
AravindhaSuresh <aravindha.c.m@gmail.com>
2022-06-27 11:10:44 +0530

    First time push

---
+++ src/test/java/com/hexa/GitClassDemo.java
@@ -0,0 +1,17 @@
+package com.hexa;
+
+public class GitClassDemo {
+private void method1() {
+	System.out.println("Inside method1");
+
+}
+private void method2() {
+	System.out.println("Inside method2");
+
+}
+private void method3() {
+	System.out.println("Inside method3");
+
+}
+
+}
